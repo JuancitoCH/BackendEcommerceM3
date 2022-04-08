38105995189f798967248123fcef46c046b8e4fa
JuancitoCH <cjuan.chona@hotmail.com>
2022-04-08 01:40:15 -0300

    validate logg route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Auth = require('../services/auth')
 const cookieResponse = require('../libs/cookieResponse')
-
+const {isUser} = require('../middlewares/auth')
 const authRoutes =(app)=>{
     const router = express.Router()
     app.use('/user/service',router)
@@ -15,6 +15,15 @@ const authRoutes =(app)=>{
     router.post('/login',async(req,res)=>{
         const responseObject = await authService.loginUser(req.body)
         return cookieResponse(res,responseObject)
+    })
+    router.get('/login/validate',isUser,async(req,res)=>{
+        return res.json({success:true,user:{
+            id:req.userData.id,
+            username:req.userData.username,
+            email:req.userData.email,
+            role:req.userData.role,
+        }
+        })
     })
 
     
